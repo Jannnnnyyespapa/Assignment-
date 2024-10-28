3a1f8af0befb7a630596026eba0ab40cc0132393
Jannnnnyyespapa <jhonorland.paner@cvsu.edu.ph>
2024-10-28 16:49:47 +0800

    Completed Assignment 2: Prime Number

--- assignments/src/assignment/assignment.controller.ts
+++ assignments/src/assignment/assignment.controller.ts
@@ -17,4 +17,24 @@ export class AssignmentController {
   }
 //prime-number
 
+@Get('prime/:number')
+isPrime(@Param('number', ParseIntPipe) number: number): { Prime: boolean } {
+  const Prime = this.checkPrime(number);
+  return {Prime }; // Only return isPrime property
+}
+
+private checkPrime(num: number): boolean {
+  if (num <= 1) return false; // 0 and 1 are not prime numbers
+  if (num <= 3) return true; // 2 and 3 are prime numbers
+
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+  return true;
+}
+
+
+
 }
